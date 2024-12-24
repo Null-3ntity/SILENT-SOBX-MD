787dfe99e2aede43bafa26b12350f263193574dd
Null Entity <entitynull347@gmail.com>
2024-12-24 09:31:27 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "K6hnjQzY#Ut6es_hxxyS70bEJM-n-seQlG3j7nHqGVV5rplq2SQk",
     CAPTION: process.env.CAPTION || "POWERED BY SILENTLOVER432",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
